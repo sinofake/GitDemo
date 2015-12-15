d8a7482436828d045f849fda1a8014bb3836021f
sinofake <sinuofake@gmail.com>
2015-12-15 23:07:27 +0800

    first commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by zhucuirong on 15/12/15.
+//  Copyright © 2015年 SINOFAKE SINEP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by zhucuirong on 15/12/15.
+//  Copyright © 2015年 SINOFAKE SINEP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
