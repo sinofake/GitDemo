92b2720458ae4521d6dee912e91e8613dc61a5a5
sinofake <sinuofake@gmail.com>
2015-12-15 23:13:16 +0800

    # Xcode

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"首次提交");
 }
 
 - (void)didReceiveMemoryWarning {
